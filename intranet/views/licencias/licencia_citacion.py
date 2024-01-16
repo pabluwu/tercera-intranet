63ebfd862f872931c09266bef2cfd3758f581fe0
pabluwu <lopez.pablo2305@gmail.com>
2024-01-16 19:22:00 -0300

    Cambios Licencia

--- intranet/views/licencias/licencia_citacion.py
+++ intranet/views/licencias/licencia_citacion.py
@@ -3,18 +3,30 @@ from django.shortcuts import render
 from intranet.models import Licencia, Citacion
 from django.http import Http404
 from datetime import datetime, timedelta
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.admin import User
 
 @login_required
 def licencia_citacion(request, id_citacion):
-    current_date = datetime.now().date()
+    
+    citacion = get_object_or_404(Citacion, id=id_citacion)
+
     current_datetime = datetime.now()
 
-    
-    citaciones = Citacion.objects.filter(fecha__gt=current_date)
+    licencias = Licencia.objects.filter(citacion_id=citacion.id).select_related("autor")
+
+    for l in licencias:
+        print(l.autor)
+
+    context = {
+        'current_datetime' : current_datetime,
+        'citacion' : citacion,
+        'licencias' : licencias,
+    }
     
 
-    return render(request, './licencias/licencia_citacion.html', {"citaciones": citaciones, "current_datetime": current_datetime})
+    return render(request, './licencias/licencia_citacion.html', context)
     
     
     
